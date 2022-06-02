fdee549ee8ff512e2a8ec42b89fbdd264f250467
Arsi2022 <arsie2008@gmail.com>
2022-06-01 21:03:56 -0400

    assigment compelet

---
+++ src/main/java/virtual_Pet/VirtualPetApplication.java
@@ -0,0 +1,42 @@
+package virtual_Pet;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class VirtualPetApplication {
+
+    public static void main(String[] args) {
+        //Interact with a virtual_Pet.VirtualPet object in this method
+        VirtualPet Journey = new VirtualPet("Journey", 6, 6, 6);
+        Scanner input = new Scanner(System.in);
+        while (Journey.isAlive()); {
+            Journey.showStatus();
+            System.out.println("hit selection:feed| play| water| quit");
+            String userChoice = input.nextLine();
+            switch (userChoice.toLowerCase()) {
+                case "feed":
+                    Journey.feed();
+                    break;
+                case "water":
+                    Journey.water();
+                    break;
+                case "play":
+                    Journey.play();
+                    break;
+            }
+
+        }
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
---
+++ src/main/java/virtual_Pet/VirtualPetTest.java
@@ -0,0 +1,50 @@
+package virtual_Pet;
+
+  // import org.junit.jupiter.api.Assertions
+
+  //import org.junit.jupiter.api.Test;
+
+  //import static org.junit.jupiter.api.Assertions.assertTrue;
+
+ public class VirtualPetTest {
+
+    @Test
+    public void hungerShouldDecreaseWhenFed() {
+        VirtualPet underTest = new VirtualPet("testpet", 6,6, 4);
+        underTest.feed();
+        Assertions.assertEquals(6, underTest.getHungerLevel());
+    }
+    @Test
+    public void boredomLevelDecreaseWhenFed() {
+        VirtualPet underTest = new VirtualPet("testpet", 6, 6, 4);
+        underTest.play();
+        Assertions.assertEquals(6, underTest.getBoredomLevel());
+    }
+    @Test
+    public void thirstLevelDecreaseWhenFed() {
+        VirtualPet underTest = new VirtualPet("testpet", 6, 6, 4);
+        underTest.play();
+        Assertions.assertEquals(6, underTest.getThirstLevel());
+    }
+    @Test
+    public void hungerThirstBoredomShouldIncreaseWithTime() {
+        VirtualPet underTest = new VirtualPet ("testpet",6,6,4);
+        underTest.tick();
+        assertTrue(underTest.getHungerLevel()>6);
+        assertTrue(underTest.getBoredomLevel()>6);
+        assertTrue(underTest.getThirstLevel()>4);
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
